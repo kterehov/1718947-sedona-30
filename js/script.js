c36e933947e51f224677b29048f67d24252cbfde
Konstantin Terekhov <kterekhov@pult.ru>
2021-03-19 14:51:16 +0300

    project defense

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const dateDeparture = document.getElementById("date-departure");
 const dateArrival= document.getElementById("date-arrival");
 const nmbAdults= document.getElementById("nmb-adults");
 const nmbChildren= document.getElementById("nmb-children");
+loginPopup.classList.remove("modal-show");
 
 loginLink.addEventListener("click", function (evt) {
 
